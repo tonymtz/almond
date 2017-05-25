fc19213d3cdd4cd324c33341f67a55c37fcd27ae
Jose Salcido <eljose152@gmail.com>
2017-05-24 22:16:50 -0700

    testify - TestInit (noError)

--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 import "github.com/tonymtz/almond/server"
 
 func main() {
-	server.Init()
+    server.Init()
 }
---
+++ server/router_test.go
@@ -0,0 +1,10 @@
+package server
+
+import (
+    "github.com/stretchr/testify/assert"
+    "testing"
+)
+
+func TestInit(t *testing.T) {
+    assert.NoError(t, nil, nil)
+}
