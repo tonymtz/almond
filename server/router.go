e8acdc2d280930e069d43b7fb68a221bdf0437a5
Jose Salcido <eljose152@gmail.com>
2017-05-24 20:27:40 -0700

    editorconfig

--- server/router.go
+++ server/router.go
@@ -1,15 +1,15 @@
 package server
 
 import (
-	"net/http"
-	"github.com/labstack/echo"
+    "net/http"
+    "github.com/labstack/echo"
 )
 
 func router(e *echo.Echo) {
-	/*
-	 * Hello World
-	 */
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!");
-	})
+    /*
+     * Hello World
+     */
+    e.GET("/", func(c echo.Context) error {
+        return c.String(http.StatusOK, "Hello, World!")
+    })
 }
--- server/start.go
+++ server/start.go
@@ -1,14 +1,14 @@
 package server
 
 import (
-	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
+    "github.com/labstack/echo"
+    "github.com/labstack/echo/middleware"
 )
 
 func Init() {
-	e := echo.New()
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-	router(e)
-	e.Logger.Fatal(e.Start(":3000"))
+    e := echo.New()
+    e.Use(middleware.Logger())
+    e.Use(middleware.Recover())
+    router(e)
+    e.Logger.Fatal(e.Start(":3000"))
 }
